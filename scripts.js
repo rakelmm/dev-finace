c20b730a0ff500859acbffdbcd49052b8c0423f4
rakelmm <rakelmoreiramendonca@gmail.com>
2021-03-10 17:06:47 -0300

    alteração

--- scripts.js
+++ scripts.js
@@ -18,7 +18,7 @@ const Modal = {
     }
 } 
 
-const Transaction = [
+const transaction = [
     {
         id: 1,
         description: 'luz',
@@ -58,39 +58,50 @@ const Transaction = {
     }
 }
 
+
+
 const DOM = {
     transactionsContainer: document.querySelector('#data-table tbody'), 
-    addTransaction(Transaction, index) {
+    addTransaction(transaction, index) {
         const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLTransaction(Transaction)
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
 
         DOM.transactionsContainer.appendChild(tr)
 
         
     },
-    innerHTMLTransaction(Transaction) {
+    innerHTMLTransaction(transaction) {
         const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
         const amount = Utils.formatCurrency(transaction.amount)
 
         const html = `
-        <td class="description">${Transaction.description}</td>
-        <td class="${CSSclass}">${transaction.amount}</td>
-        <td class="date">${transaction.date}</td>
-        <td>
-            <img src="./assets/minus.svg" alt="remover 
-            Transação">
-        </td>
-   
-    
-`               
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclass}">${transaction.amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td>
+                <img src="./assets/minus.svg" alt="Remover 
+                Transação">
+            </td>
+        `               
         return html
 }
 }
 
-const Utils ={
+const Utils = {
     formatCurrency(value) {
-        const signal = Number(value) < 0 ? "-" : ""
+        const signal = Number(value) < 0 ? "-" : "" 
+
+        value = String(value).replace(/\D/g, "")
+
+        value = Number(value) / 100
+
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+
+        console.log(value)
     }
 }
 
